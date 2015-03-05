c6392da6ee9e5844f1f421cdd27e8fcf77a1ab6e
Luka Domitrovic <luka@domitrovic.si>
2015-03-05 21:35:06 +0100

    add devise gem

--- Gemfile
+++ Gemfile
@@ -31,3 +31,5 @@ group :test do
   gem 'factory_girl_rails'
   gem 'ffaker'
 end
+
+gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'api_constraints'
 Rails.application.routes.draw do
 
+  devise_for :users
   namespace :api, defaults: { format: :json }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       # We are going to list our resources here
