56cc6b0af62f1a995bcd450868cfb97764246f1b
Luka Domitrovic <luka@domitrovic.si>
2015-03-05 01:23:58 +0100

    add versions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
+require 'api_constraints'
 Rails.application.routes.draw do
 
-
+  namespace :api, defaults: { format: :json }, path: '/' do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      # We are going to list our resources here
+    end
+  end
 
 end
